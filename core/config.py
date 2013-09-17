bc07531a3b8d8e41ecfe00e23216e591e7d32b43
sheepherd <sheepherd2k@gmail.com>
2013-09-17 17:54:14 +0200

    can now check the config file

--- core/config.py
+++ core/config.py
@@ -1,19 +1,24 @@
+#!/usr/bin/python
+
 import configparser
 
 
 class Configuration:
     def __init__(self):
-        config = configparser.ConfigParser
+        config = configparser.ConfigParser()
 
 
     def check_configfile(configfile):
-        if config.read( == []:
-            config['basic']['Path'] = '\\infsaa1012\ecprog_kba$\Outlook_Synch_Log\Logbuch.txt'
-            config.write(configfile)
+        if config.read(configfile) == []:
+            config['basic'] = {'Path': ''}
+            with open('config.ini', 'w') as configfile:
+                config.write(configfile)
+            print('no config')
         else:
+            print('config existent')
             return True
 
 
-config = configparser.ConfigParser
+config = configparser.ConfigParser()
 configfile = 'config.ini'
 Configuration.check_configfile(configfile)
