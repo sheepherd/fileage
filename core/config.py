1e4aab83a40a58be585b20cf7a7e0df183451a49
sheepherd <sheepherd2k@gmail.com>
2013-09-23 14:30:51 +0200

    fileage check of multiple files implemented

--- core/config.py
+++ core/config.py
@@ -6,15 +6,21 @@ import configparser
 
 class Configuration:
     def __init__(self):
-        configfile = 'config.ini'
-        self.path = self.check_configfile(configfile)
+        configfile = 'config.ini'   
+        self.path = self.check_configfile(configfile)       
 
     def check_configfile(self, configfile):
         config = configparser.ConfigParser()
+        options = []
         if config.read(configfile) == []:
-            config['basic'] = {'path': 'dummyfile.ini'}
+            config['path'] = {'path1': 'dummyfile.ini'}
             with open('config.ini', 'w') as configfile:
                 config.write(configfile)
-                return config['basic']['path']
+                return[option for option in config['path'].values()]
         else:
-            return config['basic']['path']
+            return[option for option in config['path'].values()]
+             
+                
+if __name__ == '__main__':
+    x = Configuration()
+    print(x.path)
