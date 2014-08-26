ab4a7c992ed758718a803ed76a5bd0b9797a9861
sheepherd <sheepherd@sheepherd.org>
2014-08-26 21:13:48 +0200

    Fixes #1 lots of refactoring

--- core/setup.py
+++
@@ -1,7 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "File Age",
-    version = "0.1",
-    description = "Shows the age of files",
-    executables = [Executable("fileage.py", base="Win32GUI")])
